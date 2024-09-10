22a7e17c5f6a8edac916ad7a9d5249514a05819e
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2024-09-10 18:22:14 +0200

    Fix titles of index.md pages
    Issue: https://github.com/RiverBench/RiverBench/issues/117

---
+++ hooks/fix_title.py
@@ -0,0 +1,23 @@
+import logging, re
+import mkdocs.plugins
+from mkdocs.structure.pages import Page
+
+
+log = logging.getLogger('mkdocs')
+
+
+re_title = re.compile(r'^# (.+)$', re.MULTILINE)
+
+
+# Resolves: https://github.com/RiverBench/RiverBench/issues/117
+def on_page_markdown(markdown: str, page: Page, config, files) -> str:
+    if page.file.src_path.endswith('index.md'):
+        match = re_title.search(markdown)
+        if match:
+            title = match.group(1)
+            log.info(f'Fixing title of {page.file.src_path} to "{title}"')
+            page.title = title
+        else:
+            log.warning(f'Could not find title in {page.file.src_path}')
+        
+    return markdown
