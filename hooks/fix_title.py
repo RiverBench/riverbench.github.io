07fd44c671d65a0c02ecefeec01646455a48b769
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2024-09-11 10:38:59 +0200

    Fix ugly nav titles for datasets

--- hooks/fix_title.py
+++ hooks/fix_title.py
@@ -15,8 +15,10 @@ def on_page_markdown(markdown: str, page: Page, config, files) -> str:
         match = re_title.search(markdown)
         if match:
             title = match.group(1)
-            log.info(f'Fixing title of {page.file.src_path} to "{title}"')
-            page.title = title
+            # Don't fix dataset titles, they are fine as-is
+            if not title.startswith('Dataset: '):
+                log.info(f'Fixing title of {page.file.src_path} to "{title}"')
+                page.title = title
         else:
             log.warning(f'Could not find title in {page.file.src_path}')
         
